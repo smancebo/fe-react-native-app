decef66c5eb9682b2ef1d7bb0893989122f5a192
Samuel J Mancebo <smancebo@cosefi.com>
2017-12-15 17:39:01 -0400

    started pure react-native app

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View, ToolbarAndroid, TextInput } from 'react-native';
+import { StyleSheet, Text, View, ToolbarAndroid, TextInput, AppRegistry } from 'react-native';
 import { StackNavigator, DrawerNavigator } from 'react-navigation'
 import HomeScreen from './screens/HomeScreen/HomeScreen';
 import BrowseScreen from './screens//BrowseScreen/BrowseScreen';
 import { Drawer as DrawerContent } from './components/DrawerComponent';
 import { withLoading } from './components/hoc/withLoading';
 
-import { AppLoading, Constants } from 'expo';
+
 import {drawerActiveBackgroundColor, drawerBackgroundColor, drawerInactiveTintColor, drawerActiveTintColor} from './common/constants';
 
 
@@ -41,10 +41,10 @@ export default class App extends React.Component {
     fontsLoaded: false
   }
   async componentWillMount() {
-    await Expo.Font.loadAsync({
-      'Roboto': require('native-base/Fonts/Roboto.ttf'),
-      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
-    });
+    // await Expo.Font.loadAsync({
+    //   'Roboto': require('native-base/Fonts/Roboto.ttf'),
+    //   'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+    // });
     this.setState({ fontsLoaded: true })
   }
   render() {
@@ -53,16 +53,10 @@ export default class App extends React.Component {
       return (
         <Navigator />
       )
-    } else {
-      return <AppLoading />
-    }
+    } 
 
   }
 }
 
-const styles = StyleSheet.create({
-  main: {
-    marginTop: Constants.statusBarHeight,
-    backgroundColor: 'steelblue'
-  }
-});
+AppRegistry.registerComponent('ikuApp',() => App);
+
